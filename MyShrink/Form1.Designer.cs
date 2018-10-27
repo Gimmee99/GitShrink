8353b12a97b417158d1296041fe21ad54c68a952
Gimmee99 <gimmee99@hotmail.com>
2018-10-27 07:09:45 -0400

    Add files via upload

---
+++ MyShrink/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace MyShrink
+{
+    partial class frmShrink
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmShrink));
+            this.lbldraginfo = new System.Windows.Forms.Label();
+            this.listfiles = new System.Windows.Forms.ListBox();
+            this.mystrip = new System.Windows.Forms.StatusStrip();
+            this.mystatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.cmdconvert = new System.Windows.Forms.Button();
+            this.cmdclear = new System.Windows.Forms.Button();
+            this.cmdexit = new System.Windows.Forms.Button();
+            this.labelmethod = new System.Windows.Forms.Label();
+            this.listUIAlgs = new System.Windows.Forms.ComboBox();
+            this.lblamount = new System.Windows.Forms.Label();
+            this.txtamount = new System.Windows.Forms.TextBox();
+            this.txtprogress = new System.Windows.Forms.TextBox();
+            this.lblprogress = new System.Windows.Forms.Label();
+            this.cmdabout = new System.Windows.Forms.Button();
+            this.mystrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbldraginfo
+            // 
+            this.lbldraginfo.AutoSize = true;
+            this.lbldraginfo.Location = new System.Drawing.Point(12, 60);
+            this.lbldraginfo.Name = "lbldraginfo";
+            this.lbldraginfo.Padding = new System.Windows.Forms.Padding(0, 6, 0, 0);
+            this.lbldraginfo.Size = new System.Drawing.Size(130, 19);
+            this.lbldraginfo.TabIndex = 0;
+            this.lbldraginfo.Text = "Drag and Drop Files Here:";
+            // 
+            // listfiles
+            // 
+            this.listfiles.AllowDrop = true;
+            this.listfiles.FormattingEnabled = true;
+            this.listfiles.Location = new System.Drawing.Point(15, 79);
+            this.listfiles.Name = "listfiles";
+            this.listfiles.Size = new System.Drawing.Size(556, 134);
+            this.listfiles.TabIndex = 1;
+            this.listfiles.DragDrop += new System.Windows.Forms.DragEventHandler(this.listfiles_DragDrop);
+            this.listfiles.DragEnter += new System.Windows.Forms.DragEventHandler(this.listfiles_DragEnter);
+            // 
+            // mystrip
+            // 
+            this.mystrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mystatus});
+            this.mystrip.Location = new System.Drawing.Point(0, 419);
+            this.mystrip.Name = "mystrip";
+            this.mystrip.Size = new System.Drawing.Size(588, 22);
+            this.mystrip.SizingGrip = false;
+            this.mystrip.TabIndex = 2;
+            this.mystrip.Text = "statusStrip1";
+            // 
+            // mystatus
+            // 
+            this.mystatus.Name = "mystatus";
+            this.mystatus.Size = new System.Drawing.Size(42, 17);
+            this.mystatus.Text = "Ready.";
+            // 
+            // cmdconvert
+            // 
+            this.cmdconvert.Location = new System.Drawing.Point(336, 219);
+            this.cmdconvert.Name = "cmdconvert";
+            this.cmdconvert.Size = new System.Drawing.Size(75, 23);
+            this.cmdconvert.TabIndex = 3;
+            this.cmdconvert.Text = "Convert";
+            this.cmdconvert.UseVisualStyleBackColor = true;
+            this.cmdconvert.Click += new System.EventHandler(this.cmdconvert_Click);
+            // 
+            // cmdclear
+            // 
+            this.cmdclear.Location = new System.Drawing.Point(417, 219);
+            this.cmdclear.Name = "cmdclear";
+            this.cmdclear.Size = new System.Drawing.Size(73, 23);
+            this.cmdclear.TabIndex = 4;
+            this.cmdclear.Text = "Clear";
+            this.cmdclear.UseVisualStyleBackColor = true;
+            this.cmdclear.Click += new System.EventHandler(this.cmdclear_Click);
+            // 
+            // cmdexit
+            // 
+            this.cmdexit.Location = new System.Drawing.Point(496, 219);
+            this.cmdexit.Name = "cmdexit";
+            this.cmdexit.Size = new System.Drawing.Size(75, 23);
+            this.cmdexit.TabIndex = 5;
+            this.cmdexit.Text = "Exit";
+            this.cmdexit.UseVisualStyleBackColor = true;
+            this.cmdexit.Click += new System.EventHandler(this.cmdexit_Click);
+            // 
+            // labelmethod
+            // 
+            this.labelmethod.AutoSize = true;
+            this.labelmethod.Location = new System.Drawing.Point(12, 20);
+            this.labelmethod.Name = "labelmethod";
+            this.labelmethod.Padding = new System.Windows.Forms.Padding(0, 6, 0, 0);
+            this.labelmethod.Size = new System.Drawing.Size(80, 19);
+            this.labelmethod.TabIndex = 0;
+            this.labelmethod.Text = "Rezize method:";
+            // 
+            // listUIAlgs
+            // 
+            this.listUIAlgs.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.listUIAlgs.FormattingEnabled = true;
+            this.listUIAlgs.Location = new System.Drawing.Point(99, 20);
+            this.listUIAlgs.Name = "listUIAlgs";
+            this.listUIAlgs.Size = new System.Drawing.Size(219, 21);
+            this.listUIAlgs.TabIndex = 1;
+            // 
+            // lblamount
+            // 
+            this.lblamount.AutoSize = true;
+            this.lblamount.Location = new System.Drawing.Point(336, 20);
+            this.lblamount.Name = "lblamount";
+            this.lblamount.Padding = new System.Windows.Forms.Padding(0, 6, 0, 0);
+            this.lblamount.Size = new System.Drawing.Size(176, 19);
+            this.lblamount.TabIndex = 2;
+            this.lblamount.Text = "Shrink to this percentage of original:";
+            // 
+            // txtamount
+            // 
+            this.txtamount.Location = new System.Drawing.Point(513, 23);
+            this.txtamount.Name = "txtamount";
+            this.txtamount.Size = new System.Drawing.Size(58, 20);
+            this.txtamount.TabIndex = 3;
+            this.txtamount.Text = "50";
+            // 
+            // txtprogress
+            // 
+            this.txtprogress.Location = new System.Drawing.Point(15, 274);
+            this.txtprogress.Multiline = true;
+            this.txtprogress.Name = "txtprogress";
+            this.txtprogress.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtprogress.Size = new System.Drawing.Size(556, 128);
+            this.txtprogress.TabIndex = 6;
+            // 
+            // lblprogress
+            // 
+            this.lblprogress.AutoSize = true;
+            this.lblprogress.Location = new System.Drawing.Point(12, 258);
+            this.lblprogress.Name = "lblprogress";
+            this.lblprogress.Size = new System.Drawing.Size(51, 13);
+            this.lblprogress.TabIndex = 7;
+            this.lblprogress.Text = "Progress:";
+            // 
+            // cmdabout
+            // 
+            this.cmdabout.Location = new System.Drawing.Point(15, 218);
+            this.cmdabout.Name = "cmdabout";
+            this.cmdabout.Size = new System.Drawing.Size(31, 23);
+            this.cmdabout.TabIndex = 8;
+            this.cmdabout.Text = "?";
+            this.cmdabout.UseVisualStyleBackColor = true;
+            this.cmdabout.Click += new System.EventHandler(this.cmdabout_Click);
+            // 
+            // frmShrink
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 441);
+            this.Controls.Add(this.cmdabout);
+            this.Controls.Add(this.lblprogress);
+            this.Controls.Add(this.txtprogress);
+            this.Controls.Add(this.listfiles);
+            this.Controls.Add(this.lbldraginfo);
+            this.Controls.Add(this.labelmethod);
+            this.Controls.Add(this.cmdconvert);
+            this.Controls.Add(this.listUIAlgs);
+            this.Controls.Add(this.cmdexit);
+            this.Controls.Add(this.lblamount);
+            this.Controls.Add(this.cmdclear);
+            this.Controls.Add(this.txtamount);
+            this.Controls.Add(this.mystrip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmShrink";
+            this.Text = "Batch Shrink Ray for Images";
+            this.mystrip.ResumeLayout(false);
+            this.mystrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbldraginfo;
+        private System.Windows.Forms.ListBox listfiles;
+        private System.Windows.Forms.StatusStrip mystrip;
+        private System.Windows.Forms.Button cmdconvert;
+        private System.Windows.Forms.Button cmdclear;
+        private System.Windows.Forms.Button cmdexit;
+        private System.Windows.Forms.Label labelmethod;
+        private System.Windows.Forms.ComboBox listUIAlgs;
+        private System.Windows.Forms.Label lblamount;
+        private System.Windows.Forms.TextBox txtamount;
+        private System.Windows.Forms.ToolStripStatusLabel mystatus;
+        private System.Windows.Forms.TextBox txtprogress;
+        private System.Windows.Forms.Label lblprogress;
+        private System.Windows.Forms.Button cmdabout;
+    }
+}
+
---
+++ MyShrink/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace MyShrink
+{
+    
+    public partial class frmShrink : Form
+    {
+        List<string> origfiles = new List<string>();
+        List<string> sizedfiles = new List<string>();
+        List<string> listAlgs = new List<string> { "Low", "High", "BiLinear (<50% only)", "BiCubic (<25% only)", "Nearest Neighbor", "HiQualityLinear", "HiQualityBiCubic" };
+
+        public frmShrink()
+        {
+            InitializeComponent();
+            SetDefaults();
+        }
+
+        public void SetDefaults()
+        {
+            listUIAlgs.DataSource = listAlgs;
+            listUIAlgs.SelectedIndex = 6;  //HiQualityBiCubic
+        }
+
+        private void cmdconvert_Click(object sender, EventArgs e)
+        {
+
+            // Validate UI choices a little bit ...
+
+            int ihowmuch = 50;
+            try
+            {
+                 ihowmuch = int.Parse(txtamount.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Please verify you have a valid percentage (1-99) specified.", "Batch Shrink", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if ((ihowmuch < 1) || (ihowmuch>99))
+            {
+                MessageBox.Show("The compression percentage must be between 1 and 99.", "Batch Shrink", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if ((listUIAlgs.SelectedIndex == 2) && (ihowmuch >49))
+            {
+                MessageBox.Show("The BiLinear compression algorithm can only be used for <50% compression.", "Batch Shrink", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if ((listUIAlgs.SelectedIndex == 3) && (ihowmuch > 24))
+            {
+                MessageBox.Show("The BiCubic algorithm can only be used for <25% compression.", "Batch Shrink", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (listfiles.Items.Count == 0)
+            {
+                MessageBox.Show("You must specify at least one file to convert.", "Batch Shrink", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            DoTheConversion(ihowmuch);
+
+        }
+
+        private void DoTheConversion(int ihowmuch)
+        {
+
+            // Pick up the Interpolation Algorithm choice from the UI
+
+            InterpolationMode mymode = new InterpolationMode();
+            mymode = InterpolationMode.HighQualityBicubic;
+
+            switch (listUIAlgs.SelectedIndex)
+            {
+                case 0:             // Low
+                    mymode = InterpolationMode.Low;
+                    break;
+                case 1:             // High
+                    mymode = InterpolationMode.High;
+
+                    break;
+                case 2:             // BiLenear
+                    mymode = InterpolationMode.Bilinear;
+
+                    break;
+                case 3:             // BiCubic
+                    mymode = InterpolationMode.Bicubic;
+
+                    break;
+                case 4:             // Nearest Neighbor
+                    mymode = InterpolationMode.NearestNeighbor;
+                    break;
+                case 5:             // HiQualityBiLinear
+                    mymode = InterpolationMode.HighQualityBilinear;
+                    break;
+                case 6:             // HiQualityBiCubic
+                    mymode = InterpolationMode.HighQualityBicubic;
+                    break;
+            }
+
+            // Loop through each file in the file list and convert it ...
+
+            foreach (var onefile in listfiles.Items)
+            {
+                mystatus.Text = "Converting " + onefile.ToString() + " ...";
+                ConvertOneFile(onefile.ToString(), ihowmuch, mymode);
+                mystatus.Text = "Done.";
+            }
+        }
+
+        private void ConvertOneFile(string oneoriginalfile, int ihowmuch, InterpolationMode modetouse)
+        {
+            Image original = Image.FromFile(oneoriginalfile);
+
+            // The resize function takes a new absoloute width/height so we have to calculate what X% of that means
+            // Take the original width and height, then multiply the UI percentage to get the new abosolutes
+            // The alg always preserves aspect ratio so you may not get an exact match
+
+
+            Size mysize = new Size();
+
+            float ratio = (float)ihowmuch / 100;
+            float rwidth = original.Width * ratio;
+            float rheight = original.Height * ratio;
+
+            mysize.Width = (int)rwidth;
+            mysize.Height = (int)rheight;
+
+            // Call the resize code to do the work
+            Image resized = ResizeImage(original, mysize, modetouse);
+
+            // New file name is old file name plus 'resized' at the end. Save the new file.
+            string newfilename = oneoriginalfile + "_resized.jpg";
+            resized.Save(newfilename, ImageFormat.Jpeg);
+
+            // Put up a little progress and status so people know things are working ...
+            mystatus.Text = newfilename + " resized successfully.";
+            txtprogress.AppendText(newfilename + " resized successfully.\n");
+            newfilename = "";
+            Application.DoEvents();
+        }
+
+        private void listfiles_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                foreach (string filePath in files)
+                {
+                    origfiles.Add(filePath);
+                }
+                listfiles.DataSource = origfiles;
+            }
+        }
+
+        private void listfiles_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+        
+        /*
+            ResizeImage() code is adapted from http://www.codeproject.com/Articles/191424/Resizing-an-Image-On-The-Fly-using-NET
+        */
+        
+        public static Image ResizeImage(Image image, Size size, InterpolationMode mode,bool preserveAspectRatio = true )
+        {
+            int newWidth;
+            int newHeight;
+            
+            if (preserveAspectRatio)
+            {
+                int originalWidth = image.Width;
+                int originalHeight = image.Height;
+                float percentWidth = (float)size.Width / (float)originalWidth;
+                float percentHeight = (float)size.Height / (float)originalHeight;
+                float percent = percentHeight < percentWidth ? percentHeight : percentWidth;
+                newWidth = (int)(originalWidth * percent);
+                newHeight = (int)(originalHeight * percent);
+            }
+            else
+            {
+                newWidth = size.Width;
+                newHeight = size.Height;
+            }
+            Image newImage = new Bitmap(newWidth, newHeight);
+            using (Graphics graphicsHandle = Graphics.FromImage(newImage))
+            {
+                // Set in the mode that was chosen in the UI
+                graphicsHandle.InterpolationMode = mode;
+
+                // Note that the smoothing mode is always static. TODO: Add this choice to the UI as well
+                graphicsHandle.SmoothingMode = SmoothingMode.HighQuality;
+                graphicsHandle.DrawImage(image, 0, 0, newWidth, newHeight);
+            }
+            return newImage;
+        }
+
+        private void cmdclear_Click(object sender, EventArgs e)
+        {
+            // Clear UI for another pass
+            listfiles.DataSource = null;
+            listfiles.Items.Clear();
+            origfiles.Clear();
+            txtprogress.Clear();
+            mystatus.Text = "Ready";
+
+        }
+
+        private void cmdexit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void cmdabout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Scott Bradley (gimmee99@hotmail.com)" + "\n" + "Version: " + ProductVersion.ToString() + " April 2016","Sample Batch Image Resizing Tool", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
